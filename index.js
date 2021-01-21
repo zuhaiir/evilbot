c738571e359f077c93cf25f9061ea3e05a88a3fa
Zuhair <zuhairmehdee@gmail.com>
2021-01-21 07:34:21 +0600

    fixed erros

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ const insults = [
     "You're so poor",
     "You're ugly",
     "Stinky",
-    "Hey ugly"
-    "Do you realize that people just tolerate you?"
-    "You’re not pretty enough to be this stupid."
-    "If I wanted to kill myself, I’d climb to your ego and jump to your IQ."
-    "What doesn’t kill you… disappoints me."
-    "Your asshole must be so jealous of your mouth from all the shit it’s been spewing."
-    "Somewhere out there, a tree is tirelessly producing oxygen for you.  I think you owe it an apology."
+    "Hey ugly",
+    "Do you realize that people just tolerate you?",
+    "You’re not pretty enough to be this stupid.",
+    "If I wanted to kill myself, I’d climb to your ego and jump to your IQ.",
+    "What doesn’t kill you… disappoints me.",
+    "Your asshole must be so jealous of your mouth from all the shit it’s been spewing.",
+    "Somewhere out there, a tree is tirelessly producing oxygen for you.  I think you owe it an apology.",
     "It's better to let someone think you are an idiot than to open your mouth and prove it."
 ]
 
