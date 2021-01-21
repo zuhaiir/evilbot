57475da25bebddeb9062c755b0d411e4ac084df5
ssnoozes <77760996+ssnoozes@users.noreply.github.com>
2021-01-20 21:08:07 -0500

    Update index.js (#5)

--- index.js
+++ index.js
@@ -29,7 +29,19 @@ const insults = [
     "What doesn’t kill you… disappoints me.",
     "Your asshole must be so jealous of your mouth from all the shit it’s been spewing.",
     "Somewhere out there, a tree is tirelessly producing oxygen for you.  I think you owe it an apology.",
-    "It's better to let someone think you are an idiot than to open your mouth and prove it."
+    "It's better to let someone think you are an idiot than to open your mouth and prove it.",
+    "You are the human equivalent of a participation award.",
+    "You're impossible to underestimate.",
+    "I'd challenge you to a battle of wits, but I see you're unarmed.",
+    "You look like a visible fart.",
+    "I envy people who haven't met you.",
+    "You will be utterly forgotten.",
+    "You’ll go far someday. Hopefully, you’ll stay there.",
+    "Your mouth stinks.",
+    "Even Bob Ross would call you a mistake.",
+    "Your mom told you that you could become anything. Yet you still chose to become a disappointment.",
+    "You are so basic, even if you were dropped in indrustrial-grade acid, you'd balance the pH.",
+    "No one asked for your opinion, you abominable shit goblin."
 ]
 
 client.on('ready',() => console.log('I am ready' || process.env.PORT));
